a65a422b4cec78b2dd1bcaf93aa7756f4186961f
Devansh Gautam <devansh.gautam2110@gmail.com>
2026-06-14 00:19:00 +0530

    Backend is now connected with python services.

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const axios=require("axios")
 const upload=require("./config/multer_config");
 require("dotenv").config();
 const app=express();
-const exact_matching=require("./matching_algo/exact_matching");
-const normalise=require("./preprocessing/normalise")
 
 
 app.set("view engine","ejs")
@@ -17,48 +15,64 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/compare",upload.single("resume"),async function(req,res){
+
+    //  role entered by the user
     const role=req.body.role;
-    const resume=req.file;
+    const resume_path=req.file.path;
     // console.log({role,resume});
 
-    // const response=await axios.get("https://api.adzuna.com/v1/api/jobs/in/search/1",
-    //     {
-    //         params:{
+
+    //  calling the adzuna API to get diff jobs.
+    const response=await axios.get("https://api.adzuna.com/v1/api/jobs/in/search/1",
+        {
+            params:{
                 
-    //             app_id: process.env.ADZUNA_APP_ID, 
-    //             app_key: process.env.ADZUNA_APP_KEY,
-    //             what: role,
-    //             results_per_page: 2
-    //         }
-    // })
+                app_id: process.env.ADZUNA_APP_ID, 
+                app_key: process.env.ADZUNA_APP_KEY,
+                what: role,
+                results_per_page: 2
+            }
+    })
 
-    // const jobs=response.data.results;
-    
-    // const jobDescriptions = jobs.map(job => job.description);
+    const jobs=response.data.results;
     
 
-    const python_response=await axios.post(
-    "http://localhost:8000/extract-skills"
-    // {
-    //     resume: resume,
-    //     jobDescriptions: jobDescriptions
-    // }
-    );
+    //  it'll have the job description only
+    const jobDescriptions = jobs.map(job => job.description);
+    
+    
+     try {
 
-    const {resume_skills,job_skills}=python_response.data;
+        //  calling the python service to extract the skills from resume,job description
+        const python_response = await axios.post(
+            "http://localhost:8000/extract-skills",
+            {
+                resume_path,
+                jobDescriptions
+            }
+        );
 
 
+        const { resume_skills, job_skills } = python_response.data;
 
-    const resume_set=new Set(
-        resume_skills.map(normalise)
-    );
 
-    const {matched,missing}=exact_matching(resume_set,job_skills);
-    const matched_percentage=(matched.length/job_skills.length)*100;
+        //  after extraction again calling the python service to give the final score
+        const python_response2=await axios.post(
+            "http://localhost:8000/compare",
+                {
+                    resume_skills,
+                    job_skills
+                }
+        )
+        res.send(python_response2.data)
 
-    res.json({matched_percentage,matched,missing});
+    } catch(err) {
+        console.log("Error:", err.message);
+        console.log("Response:", err.response?.data);
+        return res.status(500).send("Python service error");
+    }
+   
+    
 })
-
-
 
 app.listen(3000)
